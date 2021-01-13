15318e33f52bd5db6965a0ad46a955d09764288b
Yair Chuchem <yairchu@gmail.com>
2021-01-13 14:59:53 +0200

    Add test for https://github.com/lamdu/lamdu/issues/114 and pull fix from hypertypes
    An equivalent test was also added in hypertypes

--- test/test.hs
+++ test/test.hs
@@ -1,4 +1,30 @@
+{-# LANGUAGE OverloadedStrings, TypeFamilyDependencies #-}
+
+import Control.Lens (at)
+import Control.Lens.Operators
+import Data.Set (fromList)
+import Hyper
+import Hyper.Type.AST.Scheme
+import Lamdu.Calc.Infer
+import Lamdu.Calc.Type
 import Test.Framework
+import Test.Framework.Providers.HUnit (testCase)
+import Test.HUnit (assertBool)
+
+alphaEqTest :: Test
+alphaEqTest =
+    not (alphaEq (f (TVarP "a")) (f (TRecordP REmptyP)))
+    & assertBool "should alpha eq"
+    & testCase "alpha-eq"
+    where
+        f x =
+            TFunP (TVarP "a") (TVariantP (RExtendP "t" x (RVarP "c"))) ^. hPlain
+            & Scheme
+                ( Types
+                    (QVars (mempty & at "a" ?~ mempty))
+                    (QVars (mempty & at "c" ?~ RowConstraints (fromList ["t"]) mempty))
+                )
+            & Pure
 
 main :: IO ()
-main = defaultMain []
+main = defaultMain [alphaEqTest]
